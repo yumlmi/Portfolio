a658d1533303e07b39fa644473e473374826b960
YumiYamashita <2412110324n@kindai.ac.jp>
2026-02-17 19:54:34 +0900

    ビルドの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "docs", // ← build 出力先を docs にする
+    emptyOutDir: true, // docs を上書きする
+  },
+});
